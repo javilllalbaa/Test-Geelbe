9c363dca095f7cfcec3d9568b2644d059397df00
Javier alexander Villalba <tec.informatica.javier@gmail.com>
2018-10-03 08:07:39 -0500

    First commit

---
+++ src/permutaciones/Permutaciones.java
@@ -0,0 +1,101 @@
+package permutaciones;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import java.util.Scanner;
+/**
+ *
+ * @author jvillalba
+ */
+public class Permutaciones {
+    
+    private List<String> combinaciones = new ArrayList<>();
+    private String[] words;
+
+    public Permutaciones() {
+    }
+
+    public Permutaciones(String[] words) {
+        this.words = words;
+    }
+    
+    public void GenerarPermutaciones(){
+        for(int i = 0; i < words.length; i++){
+            Getdata(words[i]);
+        }
+    }
+            
+    public void EjecutarEjemplo(){
+        String ejemplo_01 = "hat";
+        Getdata(ejemplo_01);
+        String ejemplo_02 = "abc";
+        Getdata(ejemplo_02);
+        String ejemplo_03 = "Zu6";
+        Getdata(ejemplo_03);
+    }
+    
+    public void Getdata(String word){
+        System.out.println("Permutaciones para la palabra: " + word);
+        String[] elementos = word.split(""); 
+        int r = elementos.length; // tamaño de la palabra  
+        Perm2(elementos, "", r, r); // Es importante enviar r dos veces por la recursividad n disminuye.
+        Collections.sort(combinaciones); // Ordena alfabeticamente la lista de palabras permutadas
+        for (String perm : combinaciones) {
+            System.out.println(perm);// Imprimimos las permutaciones
+        }
+    }    
+    private void Perm2(String[] elem, String act, int n, int r) {
+        if (n == 0) {
+            combinaciones.add(act); // Guarda las permutaciones
+        } else {
+            for (int i = 0; i < r; i++) {
+                if (!act.contains(elem[i])) { // Controla que no haya repeticiones
+                    Perm2(elem, act + elem[i], n - 1, r);
+                }
+            }
+        }
+    }        
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            
+	    System.out.println("1. Nuevo Arreglo de Palabras");
+	    System.out.println("2. Ejemplo Test");
+
+	    int val = sc.nextInt();
+
+	    switch (val) {
+		case 1:
+                    int n;
+                    String[] words; 
+                    System.out.print("Introduzca la cantidad de palabra ha ingresar: ");
+                    n = sc.nextInt();
+                    words = new String[n];
+                    for(int i = 0; i < n; i++){
+                        System.out.print("Introduzca una palabra: ");
+                        words[i] = sc.nextLine();
+                    }
+                    Permutaciones ob = new Permutaciones(words);
+                    ob.GenerarPermutaciones();
+		    break;
+		case 2:
+                    Permutaciones ob_ = new Permutaciones();
+                    ob_.EjecutarEjemplo();
+		    break;
+		default:
+		    //sale
+		    return;
+	    }
+	}
+        
+        
+        
+        
+
+    }
+       
+}
